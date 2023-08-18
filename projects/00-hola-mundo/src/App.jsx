42148ea2fb320dd41aa122d491b8cf970d5f6d60
Ivan Saez <ivansaezrodri@gmail.com>
2023-08-18 20:11:02 +0200

    Añadido array de usuarios para un futuro fetch

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,14 +1,43 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+    {
+        name: 'Astrid Hansen',
+        userName: 'astridhansen',
+        avatar: '68',
+        initialIsFollowing: true
+    },
+    {
+        name: 'Jennie Nickols',
+        userName: 'jennienickols',
+        avatar: '76',
+        initialIsFollowing: false
+    },
+    {
+        name: 'Catalina Diaz',
+        userName: 'catalinadiaz',
+        avatar: '49',
+        initialIsFollowing: false
+    },
+    {
+        name: 'Lily Lynch',
+        userName: 'lilylyinch',
+        avatar: '12',
+        initialIsFollowing: false
+    }
+]
+
+
 export function App() {
     return (
         <section className='App'>
             <h3 className='title'>A quién seguir</h3>
-            <TwitterFollowCard name="Astrid Hansen" userName="astridhansen" avatar="68" />
-            <TwitterFollowCard initialIsFollowing name="Jennie Nickols" userName="jennienickols" avatar="76"  />
-            <TwitterFollowCard name="Catalina Diaz" userName="catalinadiaz" avatar="49" />
-            <TwitterFollowCard name="Lily Lynch" userName="lilylyinch" avatar="12" />
+            {
+                users.map( ({name, userName, avatar, initialIsFollowing}) => (
+                    <TwitterFollowCard key={userName} name={name} userName={userName} avatar={avatar} initialIsFollowing={initialIsFollowing} />
+                ))
+            }
         </section>
     )
 } 
