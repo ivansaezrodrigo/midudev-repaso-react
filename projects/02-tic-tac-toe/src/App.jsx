3a5a218519ea8ac0e5b0fb3f6ca11957d0ecb417
Ivan Saez <ivansaezrodri@gmail.com>
2023-08-19 13:11:25 +0200

    Logica terminada

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -11,7 +11,7 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   const className= `square ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
 
   return (
@@ -21,13 +21,50 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   const [board,setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner,setWinner] = useState(null) // null = no ganador, false = empate
 
-  const updateBoard = () => {
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if (boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[b] === boardToCheck[c]
+        ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return // no actualizamos esta posición si ya tiene algo
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // revisamos si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)      
+      alert(`El ganador es ${newWinner}`)
+    }
   }
 
   return (
