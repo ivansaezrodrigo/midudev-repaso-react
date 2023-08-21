480641689309150c5c8bde8b1a45eaa73544e8a4
Ivan Saez <ivansaezrodri@gmail.com>
2023-08-21 13:28:25 +0200

    Custom hooks creados

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,24 +1,14 @@
-import { useState, useEffect } from "react";
-import { getRandomFact } from "./services/facts.js";
+import { useCatFact } from "./hooks/useCatFact.js";
 import { useCatImage } from "./hooks/useCatImage.js";
 
 const CAT_PREFIX_IMAGE_URL = `https://cataas.com/`;
 
 export function App() {
-  const [fact, setFact] = useState();
+  const { fact, refreshFact } = useCatFact()
   const { imageUrl } = useCatImage({ fact });
 
-  // para recuperar la cita
-  useEffect(() => {
-    () => {
-      getRandomFact().then((newFact) => setFact(newFact));
-      console.log("ha funcionado");
-    };
-  }, []);
-
   const handleClick = async () => {
-    const newFact = await getRandomFact()
-    setFact(newFact)
+    refreshFact()
   }
 
   return (
