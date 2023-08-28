c41ded5c25b06579e0736c0a6d680ac30b1821e5
Ivan Saez <ivansaezrodri@gmail.com>
2023-08-28 15:04:42 +0200

    Custom hook del search y errors

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -4,10 +4,26 @@ import { Movies } from './components/Movies.jsx'
 import { useMovies } from './hooks/useMovies.js'
 import withoutResults from './mocks/no-results.json'
 
+
+function useSearch() {
+  const [search,updateSearch] = useState('')
+  const [error,setError] = useState(null)
+
+  useEffect(()=>{
+    // ZOD es una buena librería para validar datos
+    setError(null)
+    if(search.length < 3) setError('La búsqueda ha de tener al menos 3 carácteres.')
+    if(search === "") setError('No se puede buscar una película vacía.')
+    if(search.match(/^\d+$/)) setError('No se puede buscar una película con un número.')
+  },[search])
+
+  return {search, updateSearch, error}
+}
+
+
 function App() {
   const movies = useMovies()
-  const [query,setQuery] = useState('')
-  const [error,setError] = useState(null)
+  const {search, updateSearch, error} = useSearch()
 
   console.log('render')
 
@@ -21,33 +37,29 @@ function App() {
     //
     // window.FormData:
     //const fields = new window.FormData(event.target)
-    //const query = fields.get('query')
+    //const search = fields.get('search')
     //
     // Object.fromEntries:
-    //const {query} = Object.fromEntries(new window.FormData(event.target))
+    //const {search} = Object.fromEntries(new window.FormData(event.target))
 
-    console.log({query})
+    console.log({search})
   }
 
   const handleChange = (event) =>{
-    const newQuery = event.target.value
-    if (newQuery.startsWith(' ')) return
-
-    setQuery(newQuery)
-    
-    // ZOD es una buena librería para validar datos
-    setError(null)
-    if(newQuery.length < 3) setError('La búsqueda ha de tener al menos 3 carácteres.')
-    if(newQuery === "") setError('No se puede buscar una película vacía.')
-    if(newQuery.match(/^\d+$/)) setError('No se puede buscar una película con un número.')
+    const newsearch = event.target.value
+    if (newsearch.startsWith(' ')) return
+    updateSearch(newsearch)
   }
+
+
+
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input style={{border: '1px solid transparent', borderColor: error ? 'red': 'transparent'}} onChange={handleChange} value={query} type="text" name="query" placeholder='The Avengers, Star Wars, The Matrix...' />
+          <input style={{border: '1px solid transparent', borderColor: error ? 'red': 'transparent'}} onChange={handleChange} value={search} type="text" name="search" placeholder='The Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{color:'red'}}>{error}</p>}
