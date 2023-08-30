33e4672cd85538ce9e7b17583425b27b4587845e
Ivan Saez <ivansaezrodri@gmail.com>
2023-08-30 12:25:26 +0200

    Evitado que haga la misma búsqueda

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 
@@ -6,11 +6,14 @@ export function useMovies( { search }) {
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
+    const previousSearch = useRef(search)
     
     const getMovies = async () => {
+      if (search == previousSearch.current) return
       try {
         setLoading(true)
         setError(null)
+        previousSearch.current = search
         const newMovies = await searchMovies({search})
         setMovies(newMovies)
       } catch(e){
