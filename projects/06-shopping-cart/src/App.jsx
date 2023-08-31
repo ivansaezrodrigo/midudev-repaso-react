73a6e985c60597ae89b6dac85d360eacb47e1a77
Ivan Saez <ivansaezrodri@gmail.com>
2023-08-31 18:31:14 +0200

    hook creado useFilters

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,33 +1,15 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products.jsx'
 import { useContext, useState } from 'react'
-import { Filters } from './components/Filters.jsx'
 import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
 import './App.css'
 import { IS_DEVELOPMENT } from './config'
-import { FiltersContext } from './context/filters'
-
-function useFilters () {
-  const {filters, setFilters} = useContext(FiltersContext)
-
-  const filterProducts = (products) =>
-  (products.filter(product => {
-    return (
-      product.price >= filters.minPrice && (
-        filters.category == 'all' ||
-        product.category === filters.category
-      )
-    )
-  }))
-
-  return {filters, filterProducts, setFilters}
-}
-
+import { useFilters } from './hooks/useFilters'
 
 function App() {
-  const {filters, filterProducts, setFilters} = useFilters()
   const [products] = useState(initialProducts)
+  const {filters, filterProducts, setFilters} = useFilters()
 
   const filteredProducts = filterProducts(products)
 
